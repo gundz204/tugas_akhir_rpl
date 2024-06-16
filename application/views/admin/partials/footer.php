16588f308403eb682a5f454ffdf0cb5c7cbe97ac
gundz204 <panjigunawant204@gmail.com>
2024-06-16 15:48:59 +0700

    first commit

---
+++ application/views/admin/partials/footer.php
@@ -0,0 +1,6 @@
+   
+    <footer class="section p-3 bg-dark text-white">
+        <div class="text-center">&copy;  Copyright Restoran Pintar.  <?php echo date("Y"); ?> All Rights Reserved</div>
+    </footer>
+</body>
+</html>
